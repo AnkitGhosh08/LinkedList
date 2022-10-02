5e5e55d1fe615be1b9fc44673f9c0a4975424b44
Ankit <ankitGhosh456@gmail.com>
2022-10-02 17:25:34 +0530

    ADDED: Create a LinkedList

---
+++ src/com/codinclub/LinkedListMain.java
@@ -0,0 +1,8 @@
+package com.codinclub;
+
+public class LinkedListMain{
+        public static void main(String[] args) {
+                System.out.println(" Welcome to LinkedList ");
+        }
+
+}
